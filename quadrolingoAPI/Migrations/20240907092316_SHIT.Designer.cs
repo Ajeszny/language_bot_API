b01cfe8532cc04d824664e229cf584239952395c
Ajeszny <245107@edu.p.lodz.pl>
2024-09-07 11:27:34 +0200

    Every endpoint is ready

--- quadrolingoAPI/Migrations/20240907092316_SHIT.Designer.cs
+++ quadrolingoAPI/Migrations/20240907092316_SHIT.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using quadrolingoAPI.Models;
 
@@ -11,9 +12,11 @@ using quadrolingoAPI.Models;
 namespace quadrolingoAPI.Migrations
 {
     [DbContext(typeof(APIContext))]
-    partial class APIContextModelSnapshot : ModelSnapshot
+    [Migration("20240907092316_SHIT")]
+    partial class SHIT
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
